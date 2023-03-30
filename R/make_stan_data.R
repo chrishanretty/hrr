192cf3e11ab5fd9da4d41e3c3725f5a931a5a659
chris.hanretty <chris.hanretty@gmail.com>
2023-03-30 16:45:09 +0100

    Take number of ind-level predictor variable levels from levels(...), not length(unique(...))

--- R/make_stan_data.R
+++ R/make_stan_data.R
@@ -10,39 +10,39 @@ make_stan_data <- function(f, data, ps, aux, res, areavar, weightvar, threading,
     depvar <- terms(f, lhs = TRUE, rhs = FALSE)
     depvar <- all.vars(depvar)
     stan_data$ncat <- length(unique(data[, depvar]))
-    stan_data$Y <- as.numeric(factor(data[,depvar]))
-    
+    stan_data$Y <- as.numeric(factor(data[, depvar]))
+
 
 ### (3) area-level predictors
     areapreds <- terms(f, lhs = FALSE, rhs = c(FALSE, TRUE))
 
 ### Re-order the auxiliary data to match the individual level data
-    auxmatchpos <- match(data[,areavar], aux[,areavar])
+    auxmatchpos <- match(data[, areavar], aux[, areavar])
     if (any(is.na(auxmatchpos))) {
-        print(data[,areavar])
-        print(aux[,areavar])
+        print(data[, areavar])
+        print(aux[, areavar])
         stop("Auxiliary data can't be re-ordered to match values of areavar in data")
     }
-    
-    X <- aux[auxmatchpos,]
-    mm <- model.matrix(areapreds, data = X)[,-1]
+
+    X <- aux[auxmatchpos, ]
+    mm <- model.matrix(areapreds, data = X)[, -1]
     stan_data$K_X <- ncol(mm)
     stan_data$X <- mm
-    stan_data$nAreas <- length(unique(ps[,areavar]))
+    stan_data$nAreas <- length(unique(ps[, areavar]))
 
-    rowno <- 1:nrow(ps)
-    
-    areapos_start <- tapply(rowno, ps[,areavar], min)
-    areapos_stop <- tapply(rowno, ps[,areavar], max)
+    rowno <- seq_len(nrow(ps))
 
-    stan_data$ps_area <- ps[,areavar]
-    stan_data$ps_counts <- ps[,weightvar]
+    areapos_start <- tapply(rowno, ps[, areavar], min)
+    areapos_stop <- tapply(rowno, ps[, areavar], max)
+
+    stan_data$ps_area <- ps[, areavar]
+    stan_data$ps_counts <- ps[, weightvar]
     stan_data$areastart <- areapos_start
     stan_data$areastop <- areapos_stop
 
 ### Results
-    stan_data$aggy <- res[,levels(data[,depvar])]
-    
+    stan_data$aggy <- res[, levels(data[, depvar])]
+
 ### Grainsize
     if (threading) {
         stan_data$grainsize <- round(nrow(data) / 4)
@@ -57,15 +57,15 @@ make_stan_data <- function(f, data, ps, aux, res, areavar, weightvar, threading,
     ncatvars <- length(ind_predictors)
 
     for (i in 1:ncatvars) {
-        stan_data[[paste0("N_", i)]] <- length(unique(data[,ind_predictors[i]]))
-        stan_data[[paste0("J_", i)]] <- data[,ind_predictors[i]]
+        stan_data[[paste0("N_", i)]] <- length(levels(data[, ind_predictors[i]]))
+        stan_data[[paste0("J_", i)]] <- data[, ind_predictors[i]]
     }
 
 
 ### Add on the final area categorical predictor
     i <- ncatvars + 1
-    stan_data[[paste0("N_", i)]] <- length(unique(data[,areavar]))
-    stan_data[[paste0("J_", i)]] <- data[,areavar]
+    stan_data[[paste0("N_", i)]] <- length(unique(data[, areavar]))
+    stan_data[[paste0("J_", i)]] <- data[, areavar]
     
 ### Prior_only argument
     if ("prior_only" %in% names(dots)) {
